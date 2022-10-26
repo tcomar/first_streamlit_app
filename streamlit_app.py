f86a8e497b5f14161ef293bd7b16d155bdfdd963
tcomar <116739570+tcomar@users.noreply.github.com>
2022-10-26 17:56:52 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 import pandas
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt", [Avocado, Strawberries])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.title('My parents new healthy diner')
 
